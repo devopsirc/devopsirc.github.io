8bddd8698da9e9cb6abab2fed9b9550f68ae8615
Henri Aïdasso <ahenrij@gmail.com>
2023-04-25 11:02:51 -0400

    vite conf updated

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === "production" ? "/devopsirc.github.io/" : "/",
 })
