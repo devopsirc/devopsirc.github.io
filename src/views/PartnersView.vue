9b8a02782a37c11d84ebedb44d34a1aa9aee11e7
Henri Aïdasso <ahenrij@gmail.com>
2023-04-25 19:55:21 -0400

    partners added

--- src/views/PartnersView.vue
+++ src/views/PartnersView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="team">
-    <h1>Partners logos</h1>
+  <div class="team mt-20">
+    <div class="grid grid-cols-2">
+      <div class="flex justify-center">
+        <a href="https://www.kaloom.com/" target="_blank">
+          <img src="@/assets/images/kaloom.svg" class="h-40" alt="Kaloom">
+        </a>
+      </div>
+      <div class="flex justify-center">
+        <a href="https://www.telus.com/" target="_blank">
+          <img src="@/assets/images/telus.png" class="h-24 mt-10" alt="TELUS">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
