97ac1475e8a077fbd6c98f84bbadcacceb8b344f
Henri Aïdasso <ahenrij@gmail.com>
2023-05-02 16:09:06 -0400

    home page updated

--- src/components/home/TheWelcome.vue
+++ src/components/home/TheWelcome.vue
@@ -2,36 +2,45 @@
   <div>
     <img src="@/assets/images/ets.jpg" class="h-72 w-full mt-2 md:mt-0" alt="Kaloom" />
     <h1 class="text-3xl mt-12 font-bold">Kaloom-TELUS Industrial Research Chair in DevOps</h1>
-    <h2 class="text-2xl my-4 mb-12">Helping companies react quickly and train the workforce they need</h2>
+    
+    
+    <div>
+        <h2 class="text-2xl mt-12 mb-4">Helping companies react quickly and train the workforce they need</h2>
+    
+        <p class="text-lg my-4">
+        Led by researcher Francis Bordeleau and industry partners Kaloom and TELUS, the Kaloom-TELUS
+        Chair is based on new DevOps engineering approaches and will help entrepreneurs design new IT
+        services 100 times faster than traditional methods. This allows them to react more quickly to
+        the competitive environment, which is currently marked by labour shortages.
+        </p>
 
-    <p class="text-lg my-4">
-      Led by researcher Francis Bordeleau and industry partners Kaloom and TELUS, the Kaloom-TELUS
-      Chair is based on new DevOps engineering approaches and will help entrepreneurs design new IT
-      services 100 times faster than traditional methods. This allows them to react more quickly to
-      the competitive environment, which is currently marked by labour shortages.
-    </p>
+        <p class="text-lg my-4">
+        The DevOps methodology aims to optimize the workflow by creating value for the end user, from
+        the initial idea to the deployment of the functionality. It reduces unnecessary tasks,
+        accelerates the time to market for products and services, and increases their quality. It also
+        builds on the collaboration between the various parties involved in the development of
+        reliable, high-performance products.
+        </p>
+    </div>
 
-    <p class="text-lg my-4">
-      The DevOps methodology aims to optimize the workflow by creating value for the end user, from
-      the initial idea to the deployment of the functionality. It reduces unnecessary tasks,
-      accelerates the time to market for products and services, and increases their quality. It also
-      builds on the collaboration between the various parties involved in the development of
-      reliable, high-performance products.
-    </p>
+    
+    <div>
+        <h2 class="text-2xl mt-12 mb-4">Research Interests</h2>
 
-    <p class="text-lg my-4">
-      Specifially, the research group aims at developing methods and tools to measure, analyze, and
-      improve various aspects of DevOps processes, as well as to facilitate their deployment on
-      several types of platforms and in various environments. The ongoing research works in the team cover the
-      following topics: <br />
+        <p class="text-lg my-4">
+        Specifially, the research group aims at developing methods and tools to measure, analyze, and
+        improve various aspects of DevOps processes, as well as to facilitate their deployment on
+        several types of platforms and in various environments. The ongoing research works in the team cover the
+        following topics: <br />
 
-      <ul class="list list-disc text-lg mx-8 my-4">
-        <li>Build Optimization</li>
-        <li>Intelligent DevOps (Anomaly detection, Failure diagnosis)</li>
-        <li>AI for CI/CD pipelines</li>
-        <li>Merge Request Analytics</li>
-        <li>ML4SE (Machine Learning for Software Engineening)</li>
-      </ul>
-    </p>
+        <ul class="list list-disc text-lg mx-8 my-4">
+            <li>Build Optimization</li>
+            <li>Intelligent DevOps (Anomaly detection, Failure diagnosis)</li>
+            <li>AI for CI/CD pipelines</li>
+            <li>Merge Request Analytics</li>
+            <li>ML4SE (Machine Learning for Software Engineening)</li>
+        </ul>
+        </p>
+    </div>
   </div>
 </template>
