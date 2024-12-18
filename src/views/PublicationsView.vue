ef402a7adf0db364fdd412f5eb052a38b7658cbb
Henri Aïdasso <ahenrij@gmail.com>
2024-12-17 19:46:51 -0500

    fix: publications data added

--- src/views/PublicationsView.vue
+++ src/views/PublicationsView.vue
@@ -1,17 +1,14 @@
-<template>
-  <div class="publications  mb-20">
-    <h1 class="font-bold text-3xl">Journal Articles</h1>
-    <div class="mt-4">
-      <p class="text-lg">Author, Name 1; Author, Name 2</p>
-      <p class="text-lg font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-      <p class="text-lg">In: Article venue</p>
-    </div>
+<script setup></script>
 
-    <h1 class="font-bold text-3xl mt-12">Conference Papers</h1>
-    <div class="mt-4">
-      <p class="text-lg">Author, Name 1; Author, Name 2</p>
-      <p class="text-lg font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-      <p class="text-lg">In: Article venue</p>
+<template>
+  <div class="publications mb-20">
+    <div>
+      <h1 class="font-bold text-3xl">Journal Articles</h1>
+      <div class="mt-4">
+        <p class="text-lg">Henri, Aïdasso; Mohammed, Sayagh; Francis, Bordeleau</p>
+        <p class="text-lg font-bold">Build Optimization: A Systematic Literature Review</p>
+        <p class="text-lg text-gray-500">In: ACM Computing Surveys (CSUR)</p>
+      </div>
     </div>
   </div>
 </template>
