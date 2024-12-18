d30e17df208e475bd282f40bdeee3d8279c4661f
Henri Aïdasso <ahenrij@gmail.com>
2024-12-18 12:02:22 -0500

    fix: publications data added

--- src/views/PublicationsView.vue
+++ src/views/PublicationsView.vue
@@ -1,15 +1,31 @@
 <script setup>
-
+import publications from '@/assets/data/publications.json'
 </script>
 
 <template>
   <div class="publications mb-20">
-    <div>
-      <h1 class="font-bold text-3xl">Journal Articles</h1>
-      <div class="mt-4">
-        <p class="text-lg">Henri, Aïdasso; Mohammed, Sayagh; Francis, Bordeleau</p>
-        <p class="text-lg font-bold">Build Optimization: A Systematic Literature Review</p>
-        <p class="text-lg text-gray-500">In: ACM Computing Surveys (CSUR)</p>
+    <div class="mt-8" v-for="pub in publications" :key="pub.id">
+      <div class="flex space-x-8">
+        <div class="mt-1 text-lg">{{ pub.year }}</div>
+        <div>
+          <div class="space-y-0.5">
+            <p class="text-lg">{{ pub.authors }}</p>
+            <p class="text-lg font-bold">{{ pub.title }}</p>
+            <p class="text-lg text-gray-500">{{ pub.venue }}</p>
+          </div>
+          <div class="mt-1">
+            <span
+              v-if="pub.type.toLowerCase() == 'conference'"
+              class="badge-base bg-red-600 text-red-50"
+              >{{ pub.type }}</span
+            >
+            <span
+              v-if="pub.type.toLowerCase() == 'workshop'"
+              class="badge-base bg-gray-400 text-gray-50"
+              >{{ pub.type }}</span
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
